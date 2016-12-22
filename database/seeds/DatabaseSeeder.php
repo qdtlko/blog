ea5c7ccb74d026fbefd78f45157ff2868e9dc803
bwx <841916454@qq.com>
2016-12-22 10:25:52 +0800

    one

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +13,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        Model::unguard();
+        $this->call('PostTableSeeder');
+    }
+}
+
+
+class PostTableSeeder extends Seeder
+{
+    public function run()
+    {
+        App\Post::truncate();
+        factory(App\Post::class, 20)->create();
     }
 }
